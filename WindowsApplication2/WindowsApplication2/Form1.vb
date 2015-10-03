f51bf08fb89883ed58d761cc4b78360410bb95c6
Danang Kusuma <kdanang20@yahoo.co.id>
2015-10-04 03:11:20 +0700

    FormGantiWarna
    Maaf pak telat, baru reparasi Laptop

---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class FormGanti
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub gantiButton_Click(sender As Object, e As EventArgs) Handles gantiButton.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Red
+        Else
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+End Class
